8f11729a2a7740b58cb6c69ee48a9aebb3e3f287
Manuel Salvadores <patriciozhogan@gmail.com>
2024-08-28 14:50:55 -0300

    ultimo

--- src/data/Biografias/bioAilen.js
+++ src/data/Biografias/bioAilen.js
@@ -2,7 +2,7 @@ export const bioAilen = {
   id: 'bio-ailen',
   orden: 7,
   name: 'Licenciada Ailén Mastrolia (Referente)',
-  img: '/equipo/Sin título.jpg',
+  img: '/equipo/Ailen.jpg',
   text:
     'Es Licenciada en Psicología por la Universidad de Buenos Aires. Formó parte de diversos cursos de formación y extensión en la UBA, sobre infancias, crianza, discapacidad, psicoanálisis de niños y psicología preventiva. Realizó una especialización para tutores con enfoque psicoanalítico Docente en la Materia Introducción a la Psicología en la Diplomatura en Administración de Consorcios, a cargo de Apgra, en Universidad Nacional de Lomas de Zamora. Tiene experiencia como acompañante terapéutica en diversas áreas, mayormente en integraciones escolares y domicilios. Cuenta con experiencia interdisciplinaria. Actualmente se desempeña como referente de equipo en De espacios Acompañamientos Terapéuticos, en seguimiento y supervisión de casos y trabajo con familiares.',
 };
